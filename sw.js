a1dd557b0645938c87736e2cb56000313b74d8ef
catbinderson <andreasbindertrumpet@yahoo.de>
2026-08-08 11:37:39 +0200

    Füge Vereinsvergleich in Version 0.9.0 hinzu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ligakompakt-v0.8.0",ASSETS=["./","index.html","styles.css?v=0.8.0","app.js?v=0.8.0","manifest.webmanifest","icon.svg","version.json"];
+const CACHE="ligakompakt-v0.9.0",ASSETS=["./","index.html","styles.css?v=0.9.0","app.js?v=0.9.0","manifest.webmanifest","icon.svg","version.json"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener("message",e=>{if(e.data?.type==="SKIP_WAITING")self.skipWaiting()});
