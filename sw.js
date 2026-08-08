11818d0f46d7775ff331f4412f3532e8b0a0d007
catbinderson <andreasbindertrumpet@yahoo.de>
2026-08-08 08:44:27 +0200

    Verbessere LigaKompakt: sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,4 @@
-const CACHE="ligakompakt-v0.3-ultra";
-const ASSETS=["./","index.html","styles.css?v=0.3","app.js?v=0.3","manifest.webmanifest"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{
-  if(new URL(e.request.url).hostname==="api.openligadb.de"){
-    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-    return;
-  }
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+const CACHE="ligakompakt-v0.4",ASSETS=["./","index.html","styles.css?v=0.4","app.js?v=0.4","manifest.webmanifest","icon.svg"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;const api=new URL(e.request.url).hostname==="api.openligadb.de";if(api){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));return}e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
